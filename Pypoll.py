d1d8df99b3475f2b294a900626c7eeb9a0735118
lynnokang <74890100+lynnokang@users.noreply.github.com>
2021-01-09 16:41:45 -0500

    Getting values from a list of Dictionaries

--- Pypoll.py
+++ Pypoll.py
@@ -20,6 +20,30 @@ for county in counties:
     counties_dict ={"Arapahoe":422829, "Denver":463353,"Jefferson":432438}
     for county in counties_dict:
         print(county)
+    for county in counties_dict.keys():
+        print(county)
+    for voters in counties_dict.values():
+        print(voters)
+    for county in counties_dict:
+        print(counties_dict[county])
+
+    voting_data = [{"county":"Arapahoe", "registered_voters":422829},
+        {"county":"Denver", "registered_voters": 463353},
+        {"county":"Jefferson","registered_voters": 432438}]
+    for county_dict in voting_data:
+            print(county_dict)
+    for  county_dict in voting_data:
+        for value in county_dict.values():
+            print(value)
+            print(county_dict["registered_voters"])
+        for county_dict in voting_data:
+            print(county_dict["county"])
+
+           
+
+
+
+
 
 
 
