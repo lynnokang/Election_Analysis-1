31af928244bb61fc1a85642de14122519cd101c5
lynnokang <74890100+lynnokang@users.noreply.github.com>
2021-01-08 20:39:24 -0500

    testing

---
+++ Pypoll.py
@@ -0,0 +1,5 @@
+# The data we need to retrieve
+#The total number of vote cast
+#A complete list of candidates who received votes
+# The total number of votes each candidates won
+#The winner of the election based on popular vote
