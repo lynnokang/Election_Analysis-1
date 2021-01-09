2fe80233b200952e4d20fc7f6c09e5d9d09c48d7
lynnokang <74890100+lynnokang@users.noreply.github.com>
2021-01-09 02:45:27 -0500

    still if/ else

--- Pypoll.py
+++ Pypoll.py
@@ -10,11 +10,11 @@ if "El Paso" in counties:
   print("El Paso is in the list of counties.")
 else:
     print("El Paso is not the list of counties.")
-    if "Arapahoe" in counties and "El Paso" in counties:
-        print ("Arapahoe and El Paso are int he list of counties.")
+    if "Arapahoe" in counties and "El Paso" not in counties:
+        print (" Only Arapahoe is in the list of counties.")
     else:
-        print("Arapahoe or El Paso is not in the list of counties.")
-        
+        print("Arapahoe is in the list of counties and El Paso is not in the list of counties.")
+
 
 
 
