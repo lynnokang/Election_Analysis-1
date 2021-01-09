0485f9a7cf9f2b14e5c7e9573880c2e58b1dcb5c
lynnokang <74890100+lynnokang@users.noreply.github.com>
2021-01-08 20:46:35 -0500

    uploading 1

--- Pypoll.py
+++ Pypoll.py
@@ -1,3 +1,6 @@
+print("Hello World")
+counties =["Arapahoe","Denver","Jefferson"]
+print (counties)
 # The data we need to retrieve
 #The total number of vote cast
 #A complete list of candidates who received votes
