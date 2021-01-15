2d5a6d7a45177393c0f0c60ac752a42bd8684997
lynnokang <74890100+lynnokang@users.noreply.github.com>
2021-01-15 02:10:24 -0500

    Getting names of election candidates

--- Pypoll2.py
+++ Pypoll2.py
@@ -102,19 +102,56 @@ with open(file_to_load) as election_data:
     # Read and print the header row.
     headers = next(file_reader)
     print(headers)
-    
+ 
+   # Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
 
+    # Read the header row.
+    headers = next(file_reader)
 
+    # Print each row in the CSV file.
+    for row in file_reader:
+        print(row)
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# 1. Initialize a total vote counter.
+total_votes = 0
+# Candidate Options
+candidate_options = []
 
+# Open the election results and read the file
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
 
+    # Read the header row.
+    headers = next(file_reader)
 
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # 2. Add to the total vote count.
+        total_votes += 1
+# Print the candidate name from each row
+        candidate_name = row[2]
 
-
-
-
-
+         # Add the candidate name to the candidate list.
+        candidate_options.append(candidate_name)
+# Print the candidate list.
+print(candidate_options)
 
 
 
