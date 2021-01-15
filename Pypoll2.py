47bad977c40d38c4de0eaa85ce646ae99a47e31f
lynnokang <74890100+lynnokang@users.noreply.github.com>
2021-01-14 23:53:20 -0500

    just  an update

--- Pypoll2.py
+++ Pypoll2.py
@@ -1,5 +1,7 @@
 import csv
+dir(csv)
 
+import os
 # The data we need to retrieve
 #The total number of vote cast
 #A complete list of candidates who received votes
@@ -7,6 +9,7 @@ import csv
 #The winner of the election based on popular vote
 # Import the datetime class from the datetime module.
 import datetime
+
 # Use the now() attribute on the datetime class to get the present time.
 now = datetime.datetime.now()
 # Print the present time.
@@ -14,17 +17,50 @@ print("The time right now is ", now)
 
 # Import the datetime class from the datetime module.
 import datetime as dt
+
 # Use the now() attribute on the datetime class to get the present time.
 now = dt.datetime.now()
 # Print the present time.
-print("The time right now is ", now)
+print("the time right now is", now)
+file_to_load ="Resources/election_results.csv"
+election_data = open(file_to_load, "r")
+election_data.close()
+with open(file_to_load) as election_data:
+    print(election_data)
 import csv
 import os
 file_to_load = os.path.join("Resources", "election_results.csv")
-file_to_save = os.path.join("analysis", "election_analysis -1.txt")
 with open(file_to_load) as election_data:
-    file_reader = csv.reader(election_data)
-    headers =next(file_reader)
+    print(election_data)
+file_to_save = os.path.join("Analysis", "election_analysis.txt")
+open(file_to_save, "w")
+
+file_to_save =os.path.join("analysis", "election_analysis.txt")
+outfile = open(file_to_save, "w")
+outfile.write("Hello World")
+
+outfile.close()
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
 
 
