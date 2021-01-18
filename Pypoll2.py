a15951b121b4d759714d018bf9d22b712265d223
lynnokang <74890100+lynnokang@users.noreply.github.com>
2021-01-18 01:56:05 -0500

    candidate names and votes

--- Pypoll2.py
+++ Pypoll2.py
@@ -1,6 +1,4 @@
 import csv
-dir(csv)
-
 import os
 # The data we need to retrieve
 #The total number of vote cast
@@ -148,19 +146,67 @@ with open(file_to_load) as election_data:
 # Print the candidate name from each row
         candidate_name = row[2]
 
+    # Add the candidate name to the candidate list.
+        candidate_options.append(candidate_name)
 # If the candidate does not match any existing candidate...
         if candidate_name not in candidate_options:
             # Add it to the list of candidates.
             candidate_options.append(candidate_name)
 
+# Print the candidate list.
+print(candidate_options)
+
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Initialize a total vote counter.
+total_votes = 0
+
+# Candidate options and candidate votes
+candidate_options = []
 # 1. Declare the empty dictionary.
-candidate_option =[]
 candidate_votes = {}
- # 2. Begin tracking that candidate's vote count.
-candidate_votes[candidate_name] = 0
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    # Read the header row.
+    headers = next(file_reader)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # Add to the total vote count.
+        total_votes += 1
+
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        if candidate_name not in candidate_options:
+
+           # Add the candidate name to the candidate list.
+            candidate_options.append(candidate_name)
+           # 2. Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+             # Add a vote to that candidate's count.
+            candidate_votes[candidate_name] += 1
+    
 
 # Print the candidate vote dictionary.
 print(candidate_votes)
+
+
+
+
+
+
+
+
 
 
 
